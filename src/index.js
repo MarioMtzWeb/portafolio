6d42ba9b4b7d5bd5d34498d8c5f77288213cd8bd
Mario LapTop <themarswtf@outlook.es>
2022-05-16 17:45:21 -0500

    Agregando archivos al repo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+//Para restableser las rutas en tanto como en windows & linux
 const path = require('path');
 const app = express();
 
 //settings
 app.set('port', 5000);
+//Obtenemos la ruta de los archivos views
 app.set('views', path.join(__dirname, 'views'));
+
+app.engine('html', require('ejs').renderFile);
 app.set('view engine','ejs');
 
 //middlewares
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 //Por cada ruta pagina que necesitemos tenemos que agregar una ruta
+
+//Ruta principal
 router.get('/', (req, res) => {
-    res.render('index', {title: 'Protafolio'});
+    res.render('index.html', {title: 'Protafolio'});
+});
+//Ruta de Blog
+router.get('/blog', (req, res) => {
+    res.render('blog.html', {title: 'Blog'});
+});
+//Proyectos
+router.get('/proyectos', (req, res) => {
+    res.render('proyectos.html', {title: 'Proyectos'});
+});
+//Ruta de contacto
+router.get('/contacto', (req, res) => {
+    res.render('contacto.html', {title: 'Contacto'});
 });
 
 //Exportamos el modulo router
