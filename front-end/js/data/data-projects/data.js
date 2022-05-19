6581162c936b97e8d12daba8b03520d95e7e7f04
Mario LapTop <themarswtf@outlook.es>
2022-05-19 13:17:44 -0500

    Agreando cambios seccion de proyectos

---
+++ front-end/js/data/data-projects/data.js
@@ -0,0 +1,10 @@
+const proyectos = [
+    {
+        id: 1,
+        name: 'Proyecto 1',
+        description: 'El primer proyecto que realice',
+        url: 'www.primerproyecto.com',
+        repo: 'www.github.com/marioMartinez/proyecto-1',
+        img: 'imagen-de-proyecto.jpj'
+    }
+]
